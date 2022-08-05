ae15256d6ac4f758a037b50610a1a958776ff251
Nick Klockenga <nick@klockenga.net>
2022-08-05 11:26:14 -0400

    update embit to 0.5.0 for better libsecp256k1 performance on pi/m1 platforms

--- rpi-seedsigner-dev/package/python-embit/python-embit.mk
+++ rpi-seedsigner-dev/package/python-embit/python-embit.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EMBIT_VERSION = 0.4.14
+PYTHON_EMBIT_VERSION = 0.5.0
 PYTHON_EMBIT_SOURCE = embit-$(PYTHON_EMBIT_VERSION).tar.gz
-PYTHON_EMBIT_SITE = https://files.pythonhosted.org/packages/f4/c9/133e63ba6f1da1ba684714ffef50c4e952137c04170627a9e10de42d7e2e
+PYTHON_EMBIT_SITE = https://files.pythonhosted.org/packages/d5/be/9be9bba669e901d0318e8367fd9f744fe8d6eff3ba1bcfb03f5cfa553310
 PYTHON_EMBIT_LICENSE = MIT
 PYTHON_EMBIT_SETUP_TYPE = setuptools
 
