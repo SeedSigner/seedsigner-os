0fa1729969fea1614588456a3681c886aa4b5dac
Nick Klockenga <nick@klockenga.net>
2022-11-08 23:04:37 -0500

    Add to build.sh better help display and support for custom app repo/branch

--- opt/build.sh
+++ opt/build.sh
@@ -1,41 +1,60 @@
 #!/bin/bash
 
+# global variables
+cur_dir_name=${PWD##*/}
+cur_dir=$(pwd)
+seedsigner_app_repo="https://github.com/SeedSigner/seedsigner.git"
+seedsigner_app_repo_branch="0.5.1-ram"
+
 help()
 {
-  echo "Usage: build.sh [ -h | --help ]
-
-    pick a single build architecture:
-    
-    [ --pi0 ] # image only created for pi0
-    [ --pi02w ] # image only created for pi02w
-    
-    or build them all (longest compile time)
-    
-    [ -a | --all ]
-    
-    developer options:
-    
-    [ --dev ] # creates dev version of image
-    [ --no-clean ] # build without clean, make br2_external, or seedsigner app clone
-                   # allows for partial recompile; ignored when using --all
-    [ --app-repo ] # TODO: Implement
-    [ --app-branch ] # TODO: Implement
-    [ --keep-alive ] # keeps container running after build.sh completes"
+  echo "Usage: build.sh [pi board] [options]
+  
+  Pi Board: (only one allowed)
+  -a, --all         Build for all supported pi boards
+      --pi0         Build for pi0, pi0w, and pi2
+      --pi02w       Build for pi02w and pi3
+      --pi4         Build for pi4 and pi4cmio (Not Implemented Yet)
+  
+  Options:
+  -h, --help        Display a help screen and quit
+      --dev         Builds developer version of images
+      --no-clean    Leave previous build, target, and output files
+      --skip-repo   Skip pulling repo, assume rootfs-overlay/opt is populated with app code
+      --app-repo    Build image with not official seedsigner github repo
+      --app-branch  Build image with repo branch other than default
+      --keep-alive  Keeps container/script running after completeing"
   exit 2
+}
+
+download_app_repo() {
+  # remove previous opt seedsigner app repo code if it already exists
+  rm -fr ${rootfs_overlay}/opt/
+  
+  # Download SeedSigner from GitHub and put into rootfs
+  echo "cloning repo ${seedsigner_app_repo} with branch ${seedsigner_app_repo_branch}"
+  git clone --depth 1 -b "${seedsigner_app_repo_branch}" "${seedsigner_app_repo}" "${rootfs_overlay}/opt/"
+        
+  # Delete unnecessary files to save space
+  rm -rf ${rootfs_overlay}/opt/.git
+  rm -rf ${rootfs_overlay}/opt/.gitignore
+  rm -rf ${rootfs_overlay}/opt/requirements.txt
+  rm -rf ${rootfs_overlay}/opt/docs
+  rm -rf ${rootfs_overlay}/opt/README.md
+  rm -rf ${rootfs_overlay}/opt/LICENSE.md
+  rm -rf ${rootfs_overlay}/opt/enclosures
+  rm -rf ${rootfs_overlay}/opt/seedsigner_pubkey.gpg
+  rm -rf ${rootfs_overlay}/opt/setup.py
 }
 
 build_image() {
   # arguments: $1 - config name, $2 clean/no-clean - allows for 
 
   # Variables
-  seedsigner_app_repo="https://github.com/SeedSigner/seedsigner.git"
-  seedsigner_app_repo_branch="0.5.1-ram"
   config_name="${1:-pi0}"
   config_dir="./${config_name}"
   rootfs_overlay="./rootfs-overlay"
   config_file="${config_dir}/configs/pi0"
-  cur_dir_name=${PWD##*/}
-  cur_dir=$(pwd)
   build_dir="${cur_dir}/../output"
   build2_dir="${cur_dir}/../../output"
   image_dir="${cur_dir}/../images"
@@ -52,27 +71,14 @@ build_image() {
     rm -rf "${build_dir}"
     mkdir -p "${build_dir}"
     
-    # remove previous opt seedsigner app repo code if it already exists
-    rm -fr ${rootfs_overlay}/opt/
-    
-    # Download SeedSigner from GitHub and put into rootfs
-    git clone --depth 1 -b "${seedsigner_app_repo_branch}" "${seedsigner_app_repo}" "${rootfs_overlay}/opt/"
-          
-    # Delete unnecessary files to save space
-    rm -rf ${rootfs_overlay}/opt/.git
-    rm -rf ${rootfs_overlay}/opt/.gitignore
-    rm -rf ${rootfs_overlay}/opt/requirements.txt
-    rm -rf ${rootfs_overlay}/opt/docs
-    rm -rf ${rootfs_overlay}/opt/README.md
-    rm -rf ${rootfs_overlay}/opt/LICENSE.md
-    rm -rf ${rootfs_overlay}/opt/enclosures
-    rm -rf ${rootfs_overlay}/opt/seedsigner_pubkey.gpg
-    rm -rf ${rootfs_overlay}/opt/setup.py
-    
+  fi
+  
+  if [ "${3}" != "skip-repo" ]; then
+    download_app_repo
   fi
   
   # Setup external tree
-  make BR2_EXTERNAL="../${config_dir}/" O="${build_dir}" -C ./buildroot/ # 2> /dev/null > /dev/null
+  make BR2_EXTERNAL="../${config_dir}/" O="${build_dir}" -C ./buildroot/ 2> /dev/null > /dev/null
   
   cd "${build_dir}"
   make ${config_name}_defconfig
@@ -119,11 +125,20 @@ while (( "$#" )); do
   --no-clean)
     NOCLEAN=0; shift
     ;;
+  --skip-repo)
+    SKIPREPO=0; shift
+    ;;
   --keep-alive)
     KEEPALIVE=0; shift
     ;;
   --dev)
     DEVBUILD=0; shift
+    ;;
+  --app-repo=*)
+    APP_REPO=$(echo "${1}" | cut -d "=" -f2-); shift
+    ;;
+  --app-branch=*)
+    APP_BRANCH=$(echo "${1}" | cut -d "=" -f2-); shift
     ;;
   -*|--*=) # unsupported flags
     echo "Error: Unsupported flag $1" >&2
@@ -156,10 +171,29 @@ else
   CLEAN_ARG="no-clean"
 fi
 
+# Check for --no-clean argument to pass clean/no-clean to build_image function
+if ! [ -z $SKIPREPO ]; then
+  SKIPREPO_ARG="skip-repo"
+else
+  SKIPREPO_ARG="no-skip-repo"
+fi
+
+echo $SKIPREPO_ARG
+
 # Check for --dev argument to pass to build_image function
 DEVARG=""
 if ! [ -z $DEVBUILD ]; then
   DEVARG="-dev"
+fi
+
+# check for custom app repo
+if ! [ -z ${APP_REPO} ]; then
+  seedsigner_app_repo="${APP_REPO}"
+fi
+
+# check for custom app branch
+if ! [ -z ${APP_BRANCH} ]; then
+  seedsigner_app_repo_branch="${APP_BRANCH}"
 fi
 
 ###
@@ -168,18 +202,18 @@ fi
 
 # Build All Architectures
 if ! [ -z ${ALL_FLAG} ]; then
-  build_image "pi0${DEVARG}" "clean"
-  build_image "pi02w${DEVARG}" "clean"
+  build_image "pi0${DEVARG}" "clean" "${SKIPREPO_ARG}"
+  build_image "pi02w${DEVARG}" "clean" "${SKIPREPO_ARG}"
 fi
 
 # Build only for pi0, pi0w, and pi1
 if ! [ -z ${PI0_FLAG} ]; then
-  build_image "pi0${DEVARG}" "${CLEAN_ARG}"
+  build_image "pi0${DEVARG}" "${CLEAN_ARG}" "${SKIPREPO_ARG}"
 fi
 
 # build for pi02w
 if ! [ -z ${PI02W_FLAG} ]; then
-  build_image "pi02w${DEVARG}" "${CLEAN_ARG}"
+  build_image "pi02w${DEVARG}" "${CLEAN_ARG}" "${SKIPREPO_ARG}"
 fi
 
 # if build.sh makes it this far without errors, and --keep-alive flag is set, then keep container/script running
