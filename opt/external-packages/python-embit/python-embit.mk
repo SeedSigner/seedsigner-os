216d8a3b502bc4350e53868e61fd2b93851d29fc
Nick Klockenga <nick@klockenga.net>
2022-11-30 08:14:50 -0500

    update embit to 0.6.1

--- opt/external-packages/python-embit/python-embit.mk
+++ opt/external-packages/python-embit/python-embit.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EMBIT_VERSION = 0.5.0
+PYTHON_EMBIT_VERSION = 0.6.1
 PYTHON_EMBIT_SOURCE = embit-$(PYTHON_EMBIT_VERSION).tar.gz
-PYTHON_EMBIT_SITE = https://files.pythonhosted.org/packages/d5/be/9be9bba669e901d0318e8367fd9f744fe8d6eff3ba1bcfb03f5cfa553310
+PYTHON_EMBIT_SITE = https://files.pythonhosted.org/packages/9a/74/e5e213b5c6471eb56d773eb936ba14c3a4e04ab6c6069a56c0d9a0278a39
 PYTHON_EMBIT_LICENSE = MIT
 PYTHON_EMBIT_SETUP_TYPE = setuptools
 
