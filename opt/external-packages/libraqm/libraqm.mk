497d9aba6afc116ba7e00c25325ed2ee3f8fcbbf
alvroble <50918598+alvroble@users.noreply.github.com>
2025-08-30 22:31:13 +0200

    Update libraqm to 0.10.3

--- opt/external-packages/libraqm/libraqm.mk
+++ opt/external-packages/libraqm/libraqm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRAQM_VERSION = 0.10.2
+LIBRAQM_VERSION = 0.10.3
 LIBRAQM_SITE = $(call github,HOST-Oman,libraqm,v$(LIBRAQM_VERSION))
 LIBRAQM_LICENSE = MIT
 LIBRAQM_LICENSE_FILES = COPYING
